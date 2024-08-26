539c6ff7649de18f577aef07a34c45f7642c6c73
Habel <55988172+Habel2005@users.noreply.github.com>
2024-08-26 19:02:41 +0530

    server

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+const cors = require('cors'); // Import cors
+require('dotenv').config();
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors()); // Use cors middleware
+
+app.get('/', (req, res) => {
+    res.send('Server is working');
+});
+
+app.use(cors({
+    origin: '*', 
+    methods: 'GET,POST,PUT,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type,Authorization',
+}));
+
+// Create a transporter using SMTP
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+    }
+});
+
+app.post('/send-email', (req, res) => {
+    const { name, email, phone, subject, message } = req.body;
+
+    console.log('Received data:', req.body); 
+
+    const mailOptions = {
+        from: email,
+        to: process.env.EMAIL_USER, 
+        subject: `New contact form submission: ${subject}`,
+        text: `
+            Name: ${name}
+            Email: ${email}
+            Phone: ${phone}
+            Subject: ${subject}
+            Message: ${message}
+        `
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log('Error sending email:', error);
+            res.status(500).json({ success: false, error: error.message });
+        } else {
+            console.log('Email sent:', info.response);
+            res.json({ success: true });
+        }
+    });
+});
+
+const PORT = process.env.PORT || 'https://habel.freewebhostmost.com/send-email';
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
